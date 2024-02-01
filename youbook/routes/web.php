a56451ea7733574be322b69e8a4575414b0f0ae8
Maryem Khaoua <maryem.khaoua@gmail.com>
2024-02-01 14:15:08 +0100

    add some modification

--- youbook/routes/web.php
+++ youbook/routes/web.php
@@ -1,18 +1,19 @@
 <?php
 
+
+
+
+
+
+
+
+
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\BookController;
+Route::get('/', BookController::class .'@index')->name('books.index');
+Route::get('/books/create', BookController::class . '@create')->name('books.create');
+Route::post('/books', BookController::class .'@store')->name('books.store');
+Route::get('/books/{book}', BookController::class .'@show')->name('books.show');
+Route::get('/books/{book}/edit', BookController::class .'@edit')->name('books.edit');
+Route::put('/books/{book}', BookController::class .'@update')->name('books.update');
+Route::delete('/books/{book}', BookController::class .'@destroy')->name('books.destroy');
