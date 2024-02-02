721319b62895e3b75ee9bdd135da813d3e75efff
Maryem Khaoua <maryem.khaoua@gmail.com>
2024-02-02 22:45:11 +0100

    modify update function

--- youbook/routes/web.php
+++ youbook/routes/web.php
@@ -1,19 +1,12 @@
 <?php
 
-
-
-
-
-
-
-
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
+
 Route::get('/', BookController::class .'@index')->name('books.index');
 Route::get('/books/create', BookController::class . '@create')->name('books.create');
 Route::post('/books', BookController::class .'@store')->name('books.store');
-Route::get('/books/{book}', BookController::class .'@show')->name('books.show');
-Route::get('/books/{book}/edit', BookController::class .'@edit')->name('books.edit');
-Route::put('/books/{book}', BookController::class .'@update')->name('books.update');
-Route::delete('/books/{book}', BookController::class .'@destroy')->name('books.destroy');
+Route::get('/books', BookController::class .'@show')->name('books.show');
+Route::get('/books/{id}/edit', BookController::class .'@edit')->name('books.edit');
+Route::put('/books/{id}', BookController::class .'@update')->name('books.update');
+Route::delete('/books/{id}', BookController::class .'@destroy')->name('books.destroy');
